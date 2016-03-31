d2265a45bb9546b5e9389a1d1ef14796bcb6b549
Luca Quatela and Ryan Carlile <ryan@>
2016-03-31 10:39:30 -0700

    add first buisness logic for first roman numeral character

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,13 @@
 var output = "";
 
 var decimalValue = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
-var romanNumeral =  ["I","IV","V","IX","X","XL","L","XL","L","XC","C","CD","D","CM","M"]
+var romanNumeral = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"]
 
 var numberToRoman = function(number) {
-for (var index = 0; number - decimalValue[index] >= 0; index += 1){
-  output = output.concat("I");
+for (var index = 0; (number - decimalValue[index]) >= 0; index += 1){
+
 };
+ output = output.concat(romanNumeral[index - 1]);
 };
 
 /* businnes logic END */
