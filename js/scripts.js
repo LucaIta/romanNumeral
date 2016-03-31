e7a15fe5c2f50623cee4017333b5eed151fa20bb
Luca Quatela and Ryan Carlile <luca@>
2016-03-31 09:47:41 -0700

    add roman numeral repository project with user logic enabled

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+/* businnes logic START */
+
+var output = "";
+
+var decimalValue = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+var romanNumeral =  ["I","IV","V","IX","X","XL","L","XL","L","XC","C","CD","D","CM","M"]
+
+var numberToRoman = function(number) {
+for (var index = 0; number - decimalValue[index] >= 0; index += 1){
+  output = output.concat("I");
+};
+};
+
+/* businnes logic END */
+
+
+
+/* user logic START */
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault;
+    var userInput = parseInt($("#userInput").val());
+    numberToRoman(userInput);
+    alert(output);
+  })
+
+});
+
+/* user logic END */
